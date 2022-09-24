7920e9227c76d52334912d42d58c7171754fd9d6
Nandkumar Jawale <nandkumar.jawale@xpressbees.com>
2022-09-24 21:02:32 +0530

    added yargs dependency

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
-console.log(validator.isEmail('jawale.nandu@gmail.com'))
 
-console.log(chalk.green("Success!"))
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note!")
+  }
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing a note!")
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log("Reading a note!")
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function() {
+    console.log("Listing notes!")
+  }
+});
+
+console.log(yargs.argv)
